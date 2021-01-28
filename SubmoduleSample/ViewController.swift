1bf4697b182199c047c7f9ac3e9681769f7177dc
Hiroki Yoshida <h_yoshida@arsaga.jp>
2021-01-28 17:05:07 +0900

    Initial Commit

---
+++ SubmoduleSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubmoduleSample
+//
+//  Created by 吉田寛規 on 2021/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
